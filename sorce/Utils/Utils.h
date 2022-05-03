eaac35a4d2f728c7f172579db8d052f4c0e0788a
yelimpark <pyl9983@gmail.com>
2022-05-03 11:21:58 +0900

    [Update] CsvToStruct

--- sorce/Utils/Utils.h
+++ sorce/Utils/Utils.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <list>
+#include "rapidcsv.h"
 
 class Utills {
 
+public:
+	template < typename T > static void CsvToStruct(std::list<T>& structList, const char* filename);
 };
+
+template<typename T>
+inline void Utills::CsvToStruct(std::list<T>& structList, const char* filename)
+{
+    rapidcsv::Document csvData(filename, rapidcsv::LabelParams(-1, 0));
+
+    for (int i = 1; i < csvData.GetRowCount(); ++i) {
+        std::vector<std::string> row = csvData.GetRow<std::string>(i);
+        structList.push_back(T(row));
+    }
+}
